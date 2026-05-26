7f9e64928186e5e4384f7fbd66c2d9534fd00ea8
Dimitar-Totov <dimitar.totov@abv.bg>
2026-05-26 20:54:44 +0300

    Add routes with no data to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
